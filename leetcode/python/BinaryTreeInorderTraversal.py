248fb6d8ce7613b107b08e5d8e3913d71eb4fbc7
arash <ajavanma@rptu.de>
2024-04-29 19:20:11 +0200

    binary tree inorder traversal

--- leetcode/python/BinaryTreeInorderTraversal.py
+++ leetcode/python/BinaryTreeInorderTraversal.py
@@ -1,6 +1,12 @@
 from typing import Optional, List
 
+# Given the root of a binary tree, return the inorder traversal of its nodes' values.
+# example:
+# Input: root = [1,null,2,3]
+# Output: [1,3,2]
+
 # Definition for a binary tree node.
+# Python's dynamic nature and support for default arguments make it unnecessary to define a separate constructor for when only a value is given
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
