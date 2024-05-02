28e543d55d13ed4019333885f86ddaa38c6cc3cb
arash <ajavanma@rptu.de>
2024-05-02 20:30:16 +0200

    length of last word

--- leetcode/java/LengthOfLastWord.java
+++ leetcode/java/LengthOfLastWord.java
@@ -13,5 +13,7 @@ class LengthOfLastWord {
 
 // s.trim():       trims any leading or trailing whitespace from the input string
 // split("\\s+"):  uses a regular expression to split the string around sequences of one or more whitespace characters. It results in an array of words
-
+//                 \  escape
+//                 \s matches any whitespace character
+//                 +  matches one or more of the preceding element
 // words[words.length - 1]: accesses the last element in the array, which is the last word
