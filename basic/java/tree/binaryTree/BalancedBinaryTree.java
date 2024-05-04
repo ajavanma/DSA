0aed21ab3b53f1e74355746b4048f3507dd50f5d
arash <ajavanma@rptu.de>
2024-05-04 17:41:29 +0200

    balanced binary tree

---
+++ basic/java/tree/binaryTree/BalancedBinaryTree.java
@@ -0,0 +1,45 @@
+package java.tree.binaryTree;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+ class BalancedBinaryTree {
+    public boolean isBalanced(TreeNode root) {
+        return checkHeight(root) != -1;
+    }
+
+    // helper function
+    private int checkHeight(TreeNode node) {
+        if (node == null)  return 0;
+
+        
+        int leftHeight = checkHeight(node.left);
+        if (leftHeight == -1) {
+            return -1; // Left subtree is unbalanced
+        }
+
+        int rightHeight = checkHeight(node.right);
+        if (rightHeight == -1) {
+            return -1; // Right subtree is unbalanced
+        }
+
+        if (Math.abs(leftHeight - rightHeight) > 1) {
+            return -1; // difference greater than 1 => current node is unbalanced
+        }
+
+        return Math.max(leftHeight, rightHeight) + 1; // Return the height of the current node
+    }
+}
